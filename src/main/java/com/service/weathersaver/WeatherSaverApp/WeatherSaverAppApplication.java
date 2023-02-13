c51367476ba0731b92e4d4a5bf1fa48c3ae38125
Nikita Nasevich <nasevich.nik@mail.ru>
2023-02-13 13:25:53 +0300

    init project

---
+++ src/main/java/com/service/weathersaver/WeatherSaverApp/WeatherSaverAppApplication.java
@@ -0,0 +1,13 @@
+package com.service.weathersaver.WeatherSaverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSaverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSaverAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/weathersaver/WeatherSaverApp/WeatherSaverAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.weathersaver.WeatherSaverApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSaverAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
